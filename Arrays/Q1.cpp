072dee4432068ff94015b8982a4d511622e00664
Prithvi202 <imailprithvi@gmail.com>
2021-08-12 14:32:49 +0530

    Modified Arrays folder

--- Arrays/Q1.cpp
+++ Arrays/Q1.cpp
@@ -1,6 +1,13 @@
-//Striver SDE sheet link: https://docs.google.com/document/d/1SM92efk8oDl8nyVw8NHPnbGexTS9W-1gmTEYfEurLWQ/edit
-//Question title: Sort an array of 0’s 1’s 2’s without using extra space or sorting algo 
-//Leetcode Question link: https://leetcode.com/problems/sort-colors/
-//Video solution link: https://www.youtube.com/watch?v=oaVa-9wmpns&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=2
-//Striver's code in cpp: https://github.com/striver79/SDESheet/blob/main/sort012Cpp
-//Striver's code in java: https://github.com/striver79/SDESheet/blob/main/sort012Java
+/*
+Striver SDE sheet link: https://docs.google.com/document/d/1SM92efk8oDl8nyVw8NHPnbGexTS9W-1gmTEYfEurLWQ/edit
+
+Question title: Sort an array of 0’s 1’s 2’s without using extra space or sorting algo 
+
+Leetcode Question link: https://leetcode.com/problems/sort-colors/
+
+Video solution link: https://www.youtube.com/watch?v=oaVa-9wmpns&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=2
+
+Striver's code in cpp: https://github.com/striver79/SDESheet/blob/main/sort012Cpp
+
+Striver's code in java: https://github.com/striver79/SDESheet/blob/main/sort012Java
+*/
